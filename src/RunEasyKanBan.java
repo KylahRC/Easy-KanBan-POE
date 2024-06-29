5ca9d3fd208f4243785e092597cea6a5d16450d9
KylahRC <ST10435066@rcconnect.edu.za>
2024-06-29 10:42:11 +0000

    Formatting, comments

--- src/RunEasyKanBan.java
+++ src/RunEasyKanBan.java
@@ -1,8 +1,10 @@
 import javax.swing.JOptionPane; //JOptionPane used to take user input and display messages
 import java.util.Objects;
 
-public class RunEasyKanBan {
-    public static void main(String[] args) {
+public class RunEasyKanBan
+{
+    public static void main(String[] args)
+    {
 
         RegisterUser newUser = new RegisterUser();
         newUser.UserRegMethod(); // Call the method to register user details
